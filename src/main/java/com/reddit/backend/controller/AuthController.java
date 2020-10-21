a4652972e58987383c57e39501d6e6b65483c793
kamlesh <pkamleshkumar12@gmail.com>
2020-10-21 10:46:36 +0800

    user registration

---
+++ src/main/java/com/reddit/backend/controller/AuthController.java
@@ -0,0 +1,26 @@
+package com.reddit.backend.controller;
+
+import com.reddit.backend.dto.RegisterRequest;
+import com.reddit.backend.service.AuthService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@AllArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<String> signup(@RequestBody RegisterRequest registerRequest){
+        authService.signup(registerRequest);
+        return new ResponseEntity<>("User registration successful", HttpStatus.OK);
+
+    }
+}
